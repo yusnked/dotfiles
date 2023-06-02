4b9779bd2cfc4428e5c7638c3f62081af1b0e6b9
yusnked <yusnked@gmail.com>
2023-06-02 18:32:07 +0900

    存在しても読み込めなければダメなので-rに変更

--- home/private_dot_config/_b-shell/source_nix.sh
+++ home/private_dot_config/_b-shell/source_nix.sh
@@ -1,9 +1,9 @@
 # Nix multi-user mode
-if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+if [ -r '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
     source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
 fi
 # Home-manager
-if [ -e "${HOME}/.nix-profile/etc/profile.d/hm-session-vars.sh" ]; then
+if [ -r "${HOME}/.nix-profile/etc/profile.d/hm-session-vars.sh" ]; then
     source "${HOME}/.nix-profile/etc/profile.d/hm-session-vars.sh"
 fi
 
