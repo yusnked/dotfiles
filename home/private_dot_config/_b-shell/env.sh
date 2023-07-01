5b397262d85bf4eeb3a6dee28b6e265d0dfe6db1
yusnked <yusnked@gmail.com>
2023-07-01 20:13:23 +0900

    zshenv周りの整理, bash zshで共通で使える設定をより多く切り出した
    - .config/_b-shellをchezmoiignoreから除外
    - PATHにpathを重複なく追加する関数add-2pathを追加
    - 上記関数を利用してPATHの設定を_b-shell/env.shに移動
    - nixのスクリプトをsourceする設定も_b-shell/env.shに移動
    - zshenvを一本化して、テンプレート依存を削除
    - DOTFILES_OS環境変数を追加. uname -s を実行し結果を保持
    - コメントの量を減らした

--- home/private_dot_config/_b-shell/env.sh
+++ home/private_dot_config/_b-shell/env.sh
@@ -1,16 +1,38 @@
-### 言語設定
 export LC_ALL=${LC_ALL:-ja_JP.UTF-8}
 export LANG=${LANG:-ja_JP.UTF-8}
 
-### XDG Base Directory
-# ユーザー個別の設定が書き込まれるディレクトリ (/etc と類似)。
+export EDITOR=nvim
+
 export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
-# ユーザー個別の重要でない (キャッシュ) データが書き込まれるディレクトリ (/var/cache と類似)。
 export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
-# ユーザー個別のデータファイルが書き込まれるディレクトリ (/usr/share と類似)。
 export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
-# ユーザー個別の状態ファイルをが書き込まれるディレクトリ (/var/lib と類似).
 export XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
 
-export EDITOR=nvim
+export NIX_DATA_DIR="$HOME/.nix-profile/share"
+
+export DOTFILES_OS="${DOTFILES_OS:-$(uname -s)}"
+
+# PATH
+source "$XDG_CONFIG_HOME/_b-shell/add-2path.sh"
+if [[ $DOTFILES_OS == Darwin ]]; then
+    # Homebrew
+    add-2path '/usr/local/sbin' unshift exists
+    add-2path '/usr/local/bin' unshift exists
+    add-2path '/opt/homebrew/sbin' unshift exists
+    add-2path '/opt/homebrew/bin' unshift exists
+fi
+
+# Nix multi-user mode
+if [[ -r '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]]; then
+    source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+fi
+add-2path '/nix/var/nix/profiles/default/bin' unshift exists
+add-2path "$HOME/.nix-profile/bin" unshift exists
+
+# Home-manager
+if [[ -r "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh" ]]; then
+    source "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
+fi
+
+add-2path "$HOME/.local/bin" unshift exists
 
--- home/private_dot_config/_b-shell/source_nix.sh
+++
@@ -1,9 +0,0 @@
-# Nix multi-user mode
-if [ -r '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
-    source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
-fi
-# Home-manager
-if [ -r "${HOME}/.nix-profile/etc/profile.d/hm-session-vars.sh" ]; then
-    source "${HOME}/.nix-profile/etc/profile.d/hm-session-vars.sh"
-fi
-
