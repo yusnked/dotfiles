ec5d456151bb885a83d300a85be07b31309d6609
yusnked <yusnked@gmail.com>
2023-07-03 22:48:03 +0900

    [shell] ANSI SGR 8-bit colorの見本を表示する関数ansi-color-8bitを追加

--- home/private_dot_config/_b-shell/functions.sh
+++ home/private_dot_config/_b-shell/functions.sh
@@ -33,3 +33,25 @@ function nplist() {
     nix profile list | cut -d ' ' -f 1,4 --output-delimiter=' - '
 }
 
+# ANSI color
+function ansi-color-8bit() {
+    local code
+    for code in {0..255}; do
+        printf '\e[38;5;%dm%4d\e[m' $code $code
+        if ((code % 16 == 15)); then
+            printf '\n'
+        fi
+    done
+
+    cat <<EOS
+
+fg: \e[38;5;{n}m{text}\e[39m        bg: \e[48;5;{n}m{text}\e[49m
+reset: \e[0m or \e[m               ESC: \e, \033 or \x1b
+e.g.)
+EOS
+
+    echo -e ' \\e[38;5;123mhello world!\\e[0m -> \e[38;5;123mhello world!\e[0m'
+    echo -e ' \\e[48;5;226m\\e[38;5;129mhello\\e[49m world!\\e[0m -> \e[48;5;226m\e[38;5;129mhello\e[49m world!\e[0m'
+    echo -e '\nhttps://en.wikipedia.org/wiki/ANSI_escape_code#8-bit'
+}
+
