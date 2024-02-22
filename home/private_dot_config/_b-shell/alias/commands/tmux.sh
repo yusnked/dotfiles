eeb7129296303e1fc3054bdf2508fe867f8cfb43
yusnked <yusnked@gmail.com>
2024-02-22 13:04:10 +0900

    [tmux b-shell] tmuxのstatus lineを大幅に変更・tmux-mv関数を追加

--- home/private_dot_config/_b-shell/alias/commands/tmux.sh
+++ home/private_dot_config/_b-shell/alias/commands/tmux.sh
@@ -1,7 +1,19 @@
-function tmux() {
-    if [[ $# == 0 ]]; then
-        command tmux new "$DOTS_ISHELL" -l
+function tmux-mv() {
+    [[ -z $TMUX ]] && return 1
+
+    local -r kill_session="$(tmux display -p '#{session_id}')"
+    if [[ $# > 0 ]]; then
+        tmux switch-client -t "${1?}:" && tmux kill-session -t "$kill_session"
     else
-        command tmux "$@"
+        local -r sessions="$(tmux ls -f '#{==:#{session_attached},0}' -F '#S')"
+        if [[ -z $sessions ]]; then
+            echo "No unattached sessions" >&2
+            return 1
+        fi
+
+        local selected_session
+        selected_session="$(echo "$sessions" | fzf)" || return 1
+        tmux switch-client -t "${selected_session}:" &&
+            tmux kill-session -t "$kill_session"
     fi
 }
