82f9a448dd926331e823a86cb39cf8ef269c8ab9
yusnked <yusnked@gmail.com>
2024-02-28 13:51:07 +0900

    [b-shell] PATHの設定をpaths.shに分離・get-shell-path関数の修正

--- home/private_dot_config/_b-shell/env.sh
+++ home/private_dot_config/_b-shell/env.sh
@@ -11,8 +11,6 @@ export LC_TIME='C'
 
 export DOTS_OS="${DOTS_OS:-$(uname -s)}"
 
-source "$XDG_CONFIG_HOME/_b-shell/helpers.sh"
-
 # Nix multi-user mode
 if [[ -r '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]]; then
     source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
@@ -22,15 +20,6 @@ fi
 if [[ -r "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh" ]]; then
     source "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
 fi
-path-add /nix/var/nix/profiles/default/bin "$HOME/.nix-profile/bin"
 
-if [[ $DOTS_OS == Darwin ]]; then
-    # Homebrew
-    path-add /usr/local/{sbin,bin} /opt/homebrew/{sbin,bin}
-
-    export BROWSER='/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
-elif [[ $DOTS_OS == Linux ]]; then
-    path-add -m push "$HOME/.local/flatpak"
-fi
-
-path-add "$HOME/.local/bin"
+source "$XDG_CONFIG_HOME/_b-shell/helpers.sh"
+source "$XDG_CONFIG_HOME/_b-shell/paths.sh"
---
+++ home/private_dot_config/_b-shell/paths.sh
@@ -0,0 +1,13 @@
+# Nix
+path-add /nix/var/nix/profiles/default/bin "$HOME/.nix-profile/bin"
+
+if [[ $DOTS_OS == Darwin ]]; then
+    # Homebrew
+    path-add /usr/local/{sbin,bin} /opt/homebrew/{sbin,bin}
+
+elif [[ $DOTS_OS == Linux ]]; then
+    # Flatpak
+    path-add -m push "$HOME/.local/flatpak"
+fi
+
+path-add "$HOME/.local/bin"
