f92ee4f3619335d672483428032e4b545ce7e4de
yusnked <yusnked@gmail.com>
2024-03-04 10:19:02 +0900

    [shell-script home-manager] fzf用のpreview.shの改良・他
    - ディレクトリの内容の表示にezaを使用するように変更.
    - バイナリファイル以外でnkfでファイルエンコードを表示するように変更.
    - ファイルエンコードがasciiとutf-8以外の時にnkfで変換してから表示
      するように変更.
    - ヘッダーの区切りをターミナルの幅に合わせて表示するように変更.
    - ヘッダーの色をcolour240(グレー)に変更.
    - home-managerインストールリスト更新・nkfを追加・p7zipを_7zzに変更.

--- home/private_dot_config/sheldon/fzf/executable_preview.sh
+++ home/private_dot_config/sheldon/fzf/executable_preview.sh
@@ -1,45 +1,64 @@
 #!/usr/bin/env bash
-set -eu
 
-if [[ -z $1 ]]; then exit 1; fi
-target="$1"
+set -e
 
-exa="$(exa -ld --no-permissions --octal-permissions --time-style=long-iso --git "$target")"
+target="${1%/}"
+
+[[ -e $target ]] || exit
+
+readonly bat_flags='--force-colorization --number'
+readonly eza_flags='-la --color=always --git --icons --no-permissions --octal-permissions --time-style=long-iso'
+readonly header_color='240'
+
+# Header line
+readonly line_width_80='================================================================================'
+header_line="$line_width_80$line_width_80$line_width_80$line_width_80$line_width_80"
+header_line="${header_line:0:$FZF_PREVIEW_COLUMNS}"
+
 mime="$(file -b --mime "$target")"
-is_git_dir="$(git rev-parse --is-inside-work-tree 2> /dev/null || true)"
-
-if [[ -L $target ]]; then
-    status="$exa"
-elif [[ $is_git_dir == true ]]; then
-    status="$(echo $exa | cut -d' ' -f 1-6) $(basename "$target")"
-else
-    status="$(echo $exa | cut -d' ' -f 1-5) $(basename "$target")"
+if [[ ${mime##*charset=} != binary ]]; then
+    readonly nkf="$(head -n 512 "$target" 2>/dev/null | nkf --guess)"
 fi
 
 # Preview header
-cat <<END
-$status
-$mime
-================================================================================
-END
-
-if [[ -d $target ]]; then
-    if [[ -L $target ]]; then
-        ls -FL1 --color=always "$target"
-        exit 0
+echo $(
+    if [[ $target =~ / ]]; then
+        cd_dir="${target%/*}"
+        target_dir="${target##*/}"
+    else
+        cd_dir='.'
+        target_dir="$target"
     fi
-    ls -F1 --color=always "$target"
-    exit 0
+    cd "$cd_dir" 2>/dev/null
+    eza -d $eza_flags "$target_dir" 2>/dev/null
+)
+echo -e "\e[38;5;${header_color}m$mime${nkf:+ [nkf: $nkf]}\e[39m"
+echo -e "\e[38;5;${header_color}m$header_line\e[39m"
+
+if [[ ! -r $target ]]; then
+    echo '<PERMISSION DENIED>'
+    exit
 fi
 
-bat='head -n 1024 "$target" | bat --force-colorization --number -'
-
 case "$mime" in
-    *charset=binary)
-        hexyl --border none "$target" --length 16KiB
+inode/x-empty\;*)
+    echo "<EMPTY FILE>"
     ;;
-    *)
-        eval $bat
+inode/directory\;*)
+    directories="$(eza $eza_flags "$target" | head -n 512)"
+    if [[ -n $directories ]]; then
+        echo "$directories"
+    else
+        echo '<EMPTY DIRECTORY>'
+    fi
+    ;;
+*charset=binary)
+    hexyl --border none "$target" --length 16KiB
+    ;;
+*charset=us-ascii | *charset=utf-8)
+    head -n 512 "$target" | bat $bat_flags
+    ;;
+*)
+    head -n 512 "$target" | nkf | bat $bat_flags
     ;;
 esac
-
