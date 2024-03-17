c0c8aea442dae2d901dd4eaa20a3306aa2b0a1f6
yusnked <yusnked@gmail.com>
2024-03-17 16:03:45 +0900

    [b-shell] chezmoi-reinit関数を追加・shellcheckに従いコード修正

--- home/private_dot_config/_b-shell/alias/commands/chezmoi.sh
+++ home/private_dot_config/_b-shell/alias/commands/chezmoi.sh
@@ -1,18 +1,29 @@
 alias chezmoi-reset-once='chezmoi state delete-bucket --bucket=scriptState'
 
+function chezmoi-reinit() {
+    local init_file
+    init_file="$(chezmoi execute-template '{{ .chezmoi.configFile }}')"
+    if [[ -f $init_file ]]; then
+        rm "$init_file" && chezmoi init
+    else
+        return 1
+    fi
+}
+
 function chezmoi-reload() {
     chezmoi apply && sleep 0.5 && exec "$DOTS_ISHELL"
 }
 
 # Chezmoi toggle source-path <-> target-path
 function chezmoi-toggle() {
-    local dir="$(chezmoi target-path "$PWD" 2>/dev/null || chezmoi source-path "$PWD" 2>/dev/null)"
+    local dir
+    dir="$(chezmoi target-path "$PWD" 2>/dev/null || chezmoi source-path "$PWD" 2>/dev/null)"
     if [[ -z $dir ]]; then
-        if [[ $PWD == $HOME ]]; then
-            builtin cd "$(chezmoi source-path)"
+        if [[ $PWD == "$HOME" ]]; then
+            builtin cd "$(chezmoi source-path)" || return
         else
             return 1
         fi
     fi
-    builtin cd "$dir"
+    builtin cd "$dir" || return
 }
