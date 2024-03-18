2c979f020a49bba214dd5aba01518ac0469ecab3
yusnked <yusnked@gmail.com>
2024-03-18 15:45:07 +0900

    [b-shell] ghq関連関数をshellcheckに従い修正

--- home/private_dot_config/_b-shell/alias/commands/ghq.sh
+++ home/private_dot_config/_b-shell/alias/commands/ghq.sh
@@ -1,14 +1,16 @@
 function ghq-cd() {
-    local repo="$(ghq list | fzf)"
+    local repo
+    repo="$(ghq list | fzf)"
     if [[ -n $repo ]]; then
-        builtin cd "$(ghq root)/$repo"
+        builtin cd "$(ghq root)/$repo" || return
     else
         return 1
     fi
 }
 
 function ghq-home() {
-    local repo="$(ghq list | fzf)"
+    local repo
+    repo="$(ghq list | fzf)"
     if [[ -n $repo ]]; then
         xdg-open "https:/$repo" 2>/dev/null
     else
