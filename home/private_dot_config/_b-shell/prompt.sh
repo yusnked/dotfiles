5e325ca25fd8ef65fa06f021d4e712399c8f6ea3
yusnked <yusnked@gmail.com>
2024-03-29 01:07:14 +0900

    [b-shell] プロンプトを調整・bashでpipestatusを表示するように変更・他
    - bashでもpipestatusを表示するために__prompt_pipestatus関数を
      変更して共通化. bashで動かすにはbash-preexecが必要.
    - bashでzshみたいなsignals配列変数を定義するように変更.
    - starship initに --print-full-initフラグをつけるように変更.
      (bash 4.1 以上)
    - starship initの空行とコメント行をsed -iで削除してからファイルに
      保存するように変更.

--- home/private_dot_config/_b-shell/prompt.sh
+++ home/private_dot_config/_b-shell/prompt.sh
@@ -14,10 +14,11 @@ function _configure_starship_once() {
     local cache_file="$STARSHIP_CACHE/${shell_type}-prompt.sh"
     if [[ ! $cache_file -nt $STARSHIP_CONFIG ]] || ! source "$cache_file" 2>/dev/null; then
         mkdir -p "$STARSHIP_CACHE"
-        starship init $shell_type >"$cache_file"
+        starship init $shell_type --print-full-init >"$cache_file"
 
-        # Display named directories in zsh.
+        sed -i "/^\s*#/d;/^$/d" "$cache_file"
         if [[ -n $ZSH_VERSION ]]; then
+            # Display named directories in zsh.
             sed -i "/^R\?PROMPT=/s/)'$/ --logical-path \"\${(%):-%~}\"&/" "$cache_file"
         fi
 
@@ -29,16 +30,124 @@ function _configure_starship_once() {
     unset -f _configure_starship_once
 }
 
+# The order in which the following functions are hooked is important!
+# In bash, Bash-Preexec is required to run the following hooks.
+# https://github.com/rcaloras/bash-preexec
+
 # Make something like zsh's PROMPT_SP available in bash as well.
 # Note that the display may be corrupted if the terminal is not VT220 compatible.
 if [[ -n $BASH_VERSION ]]; then
-    function __prompt_output_newline() {
-        local POS
-        IFS='[;' read -r -s -d R -p $'\033[6n' -a POS
-        [[ ${POS[2]} -gt 1 ]] && printf '\033[1;7m%%\033[m\n'
+    _prompt_sp_executing=0
+    function __prompt_sp_precmd() {
+        if [[ $_prompt_sp_executing != 0 ]]; then
+            local POS
+            IFS='[;' read -r -s -d R -p $'\033[6n' -a POS
+            [[ ${POS[2]} -gt 1 ]] && printf '\033[1;7m%%\033[m\n'
+        fi
+        _prompt_sp_executing=0
     }
-    precmd_functions+=(__prompt_output_newline)
+    function __prompt_sp_preexec() {
+        _prompt_sp_executing=1
+    }
+    precmd_functions+=(__prompt_sp_precmd)
+    preexec_functions+=(__prompt_sp_preexec)
 fi
+
+# Decoration pipestatus
+function __prompt_pipestatus() {
+    local PEL PER FMTL FMTR RETVAR
+    local -a inputted_codes
+    while [[ $# -gt 0 ]]; do
+        case $1 in
+        -f)
+            local -r FMTL="${2%%\{\}*}"
+            local -r FMTR="${2#*\{\}}"
+            shift 2
+            ;;
+        -v)
+            local -r RETVAR="$2"
+            shift 2
+            ;;
+        -e)
+            local -r PEL="${2% *}"
+            local -r PER="${2#* }"
+            shift 2
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            inputted_codes+=("$1")
+            shift
+            ;;
+        esac
+    done
+    inputted_codes+=("$@")
+
+    local -rA BEGIN_COLOR=(
+        [success]="$PEL"$'\033[38;5;70m'"$PER"
+        [error]="$PEL"$'\033[38;5;124m'"$PER"
+        [signal]="$PEL"$'\033[38;5;136m'"$PER"
+        [default]="$PEL"$'\033[38;5;247m'"$PER"
+    )
+    local -r END_COLOR="$PEL"$'\033[39m'"$PER"
+
+    local status_codes status_code state
+    for status_code in "${inputted_codes[@]}"; do
+        if [[ $status_code -ge 129 && $status_code -le 159 ]]; then
+            # shellcheck disable=SC2154
+            status_codes+=" ${BEGIN_COLOR[signal]}${signals[$((status_code - 127))]}"
+        else
+            if [[ $status_code -eq 0 ]]; then
+                state=success
+            else
+                state=error
+            fi
+            status_codes+=" ${BEGIN_COLOR[$state]}$status_code"
+        fi
+    done
+    # shellcheck disable=SC2154
+    status_codes="${BEGIN_COLOR[default]}${FMTL}${status_codes# }${BEGIN_COLOR[default]}${FMTR}${END_COLOR}"
+    if [[ -n $RETVAR ]]; then
+        printf -v "$RETVAR" '%s' "$status_codes"
+    else
+        printf '%s\n' "$status_codes"
+    fi
+}
+if [[ -n $ZSH_VERSION ]]; then # zsh
+    function __prompt_pipestatus_precmd() {
+        _prompt_pipestatus_ret=$?
+        if [[ $_prompt_pipestatus_executing == 1 ]]; then
+            local copied_pipestatus=("${pipestatus[@]}")
+            [[ ${#copied_pipestatus[@]} -eq 1 ]] && copied_pipestatus=("$_prompt_pipestatus_ret")
+            __prompt_pipestatus -f '[{}]' -e '%{ %}' -v _prompt_pipestatus_var -- "${copied_pipestatus[@]}"
+        else
+            _prompt_pipestatus_var=''
+        fi
+        _prompt_pipestatus_executing=''
+    }
+elif [[ -n $BASH_VERSION ]]; then # bash with bash-preexec
+    # shellcheck disable=SC2207
+    signals=('' EXIT $(kill -l {129..159}))
+
+    function __prompt_pipestatus_precmd() {
+        if [[ $_prompt_pipestatus_executing == 1 ]]; then
+            # shellcheck disable=SC2154
+            if [[ $__bp_last_ret_value -ge 129 && ${signals[$((__bp_last_ret_value - 127))]} =~ ^(STOP|TSTP)$ ]]; then
+                __prompt_pipestatus -f '[pipestatus: {}]' -- "$__bp_last_ret_value"
+            else
+                __prompt_pipestatus -f '[pipestatus: {}]' -- "${BP_PIPESTATUS[@]}"
+            fi
+        fi
+        _prompt_pipestatus_executing=''
+    }
+fi
+function __prompt_pipestatus_preexec() {
+    _prompt_pipestatus_executing=1
+}
+precmd_functions+=(__prompt_pipestatus_precmd)
+preexec_functions+=(__prompt_pipestatus_preexec)
 
 # OSC 133
 # https://gitlab.freedesktop.org/Per_Bothner/specifications/blob/master/proposals/semantic-prompts.md
@@ -74,8 +183,5 @@ function __prompt_osc133_preexec() {
     printf '\033]133;C;\007'
     _prompt_osc133_executing=1
 }
-
-# Use Bash-Preexec to run this in Bash.
-# https://github.com/rcaloras/bash-preexec
-preexec_functions+=(__prompt_osc133_preexec)
 precmd_functions+=(__prompt_osc133_precmd)
+preexec_functions+=(__prompt_osc133_preexec)
--- home/private_dot_config/zsh/rc/prompt.zsh
+++ home/private_dot_config/zsh/rc/prompt.zsh
@@ -1,13 +1,9 @@
-autoload -Uz add-zsh-hook
 source "$XDG_CONFIG_HOME/_b-shell/prompt.sh"
 
 if [[ $COLORTERM == truecolor && ! -e "$XDG_STATE_HOME/no_starship_prompt" ]] &&
     type starship &>/dev/null; then
     # Starship
     _configure_starship_once
-
-    autoload -Uz _prompt-pipestatus-starship
-    add-zsh-hook precmd _prompt-pipestatus-starship
 else
     # git
     autoload -Uz vcs_info
@@ -23,9 +19,6 @@ else
     PROMPT='
 %B%F{yello}%n@%m%f%b:$(_prompt-truncated-path 50 2 3) %F{cyan}$vcs_info_msg_0_%f
 %B%(?|%F{040}|%F{197})%#%f%b '
-
-    autoload -Uz _prompt-pipestatus
-    add-zsh-hook precmd _prompt-pipestatus
 fi
 
 RPROMPT='$_prompt_pipestatus_var'
