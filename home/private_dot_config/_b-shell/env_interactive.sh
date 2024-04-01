396eb62f099ab21d524f3abd6e23975fb22dc372
yusnked <yusnked@gmail.com>
2024-04-01 23:27:51 +0900

    [b-shell] shellcheckに従い修正

--- home/private_dot_config/_b-shell/env_interactive.sh
+++ home/private_dot_config/_b-shell/env_interactive.sh
@@ -1,9 +1,10 @@
 export RUNEWIDTH_EASTASIAN=0
-export GPG_TTY=$TTY
+export GPG_TTY="$TTY"
 export _ZO_DATA_DIR="$XDG_DATA_HOME/zoxide"
 
 if [[ -x /proc/$$/exe ]]; then
-    export DOTS_ISHELL="$(readlink /proc/$$/exe)"
+    DOTS_ISHELL="$(readlink /proc/$$/exe)"
 else
-    export DOTS_ISHELL="$(lsof -ap$$ -dtxt -Fn | sed '3!d;s/.//;q')"
+    DOTS_ISHELL="$(lsof -ap$$ -dtxt -Fn | sed '3!d;s/.//;q')"
 fi
+export DOTS_ISHELL
