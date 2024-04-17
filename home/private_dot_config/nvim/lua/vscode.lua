b4d53776e7aa369658ad12bfb15cfce7cda0a832
yusnked <yusnked@gmail.com>
2024-04-18 01:03:19 +0900

    [vscode] キーバインドの設定とkeybindings.jsonの追加

--- home/private_dot_config/nvim/lua/vscode.lua
+++ home/private_dot_config/nvim/lua/vscode.lua
@@ -1,8 +1,32 @@
 local vscode = require('vscode-neovim')
 local keymap = vim.keymap.set
+local nvim_input = vim.api.nvim_input
+
+vim.opt.isprint = '1-255'
 
 local change_all_occurrences = function()
-    vim.api.nvim_input('i')
+    nvim_input('i')
     vscode.action('editor.action.changeAll')
 end
 keymap('n', 'g<C-r>', change_all_occurrences)
+
+local go_to_file = function()
+    vscode.action('workbench.action.quickOpen')
+end
+keymap('n', '<Leader>p', go_to_file)
+
+local command_palette = function()
+    vscode.action('workbench.action.showCommands')
+end
+keymap('n', '<Leader>P', command_palette)
+
+local open_recent = function()
+    vscode.action('workbench.action.openRecent')
+end
+keymap('n', '<Leader>r', open_recent)
+
+local add_next_occurrence = function()
+    nvim_input('i')
+    vscode.action('editor.action.addSelectionToNextFindMatch')
+end
+keymap('n', '<D-d>', add_next_occurrence)
