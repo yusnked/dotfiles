b55ce525a784e03c8cad0667e1afa4471192b373
yusnked <yusnked@gmail.com>
2024-04-20 00:54:22 +0900

    [nvim] neovimデフォルトプラグインの無効化をlazy.nvimを使った方法に変更

--- home/private_dot_config/nvim/init.lua
+++ home/private_dot_config/nvim/init.lua
@@ -1,29 +1,3 @@
--- disable bundled plugins
-local bundledPlugins = {
-    'did_indent_on',
-    'did_install_default_menus',
-    'did_install_syntax_menu',
-    -- 'did_load_filetypes',
-    'did_load_ftplugin',
-    'loaded_2html_plugin',
-    'loaded_gzip',
-    'loaded_man',
-    'loaded_matchit',
-    'loaded_matchparen',
-    -- 'loaded_netrw',
-    -- 'loaded_netrwPlugin',
-    'loaded_remote_plugins',
-    'loaded_shada_plugin',
-    'loaded_spellfile_plugin',
-    'loaded_tarPlugin',
-    'loaded_tutor_mode_plugin',
-    'loaded_zipPlugin',
-    'skip_loading_mswin',
-}
-for _, v in ipairs(bundledPlugins) do
-    vim.g[v] = 1
-end
-
 NOT_VSCODE = vim.g.vscode ~= 1
 
 require('options')
@@ -47,4 +21,30 @@ if not vim.loop.fs_stat(lazypath) then
     })
 end
 vim.opt.rtp:prepend(lazypath)
-require('lazy').setup('plugins')
+require('lazy').setup('plugins', {
+    defaults = {
+        lazy = true,
+        version = '*',
+    },
+    performance = {
+        rtp = {
+            disabled_plugins = {
+                -- 'editorconfig',
+                'gzip',
+                'health',
+                'man',
+                -- 'matchit',
+                -- 'matchparen',
+                'netrwPlugin',
+                -- 'nvim',
+                'rplugin',
+                -- 'shada',
+                'spellfile',
+                'tarPlugin',
+                'tohtml',
+                'tutor',
+                'zipPlugin',
+            },
+        },
+    },
+})
