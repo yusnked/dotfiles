f0f3821706aef0d44687016835f14bfc99e55db6
yusnked <yusnked@gmail.com>
2024-04-19 22:30:41 +0900

    [nvim] lua tableの中身を表示するhelpers.tbl_print()を追加

--- home/private_dot_config/nvim/lua/helpers.lua
+++ home/private_dot_config/nvim/lua/helpers.lua
@@ -1,20 +1,46 @@
-local helpers = {}
+local M = {}
 
-helpers.get_listed_buffers = function()
+M.get_listed_buffers = function()
     local buffers = {}
-    local len = 0
-    local vim_fn = vim.fn
-    local buflisted = vim_fn.buflisted
-
     for _, buffer in ipairs(vim.api.nvim_list_bufs()) do
-        if buflisted(buffer) == 1 then
-            len = len + 1
-            buffers[len] = buffer
+        if vim.fn.buflisted(buffer) == 1 then
+            table.insert(buffers, buffer)
         end
     end
 
     return buffers
 end
 
-return helpers
+M.tbl_tostring = function(tbl, indent, is_nest)
+    local newline = '\n'
+    if vim.fn.has('unix') ~= 1 then
+        newline = '\r\n'
+    end
+    if not indent then indent = 0 end
+    local toprint = (is_nest and '' or string.rep(' ', indent)) .. '{' .. newline
+    indent = indent + 2
+    for k, v in pairs(tbl) do
+        toprint = toprint .. string.rep(' ', indent)
+        if (type(k) == 'number') then
+            toprint = toprint .. '[' .. k .. '] = '
+        elseif (type(k) == 'string') then
+            toprint = toprint .. k .. '= '
+        end
+        if (type(v) == 'number') then
+            toprint = toprint .. v .. ',' .. newline
+        elseif (type(v) == 'string') then
+            toprint = toprint .. '\"' .. v .. '\",' .. newline
+        elseif (type(v) == 'table') then
+            toprint = toprint .. M.tbl_tostring(v, indent + 2, true) .. ',' .. newline
+        else
+            toprint = toprint .. '\"' .. tostring(v) .. '\",' .. newline
+        end
+    end
+    toprint = toprint .. string.rep(' ', indent - 2) .. '}'
+    return toprint
+end
+M.tbl_print = function(tbl)
+    print(M.tbl_tostring(tbl))
+end
 
+return M
