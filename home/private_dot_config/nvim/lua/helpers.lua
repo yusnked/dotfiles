f44fb79b3e481d128c8a62766603bed5b9549303
yusnked <yusnked@gmail.com>
2024-04-23 23:16:34 +0900

    [nvim] helpers.luaの更新

--- home/private_dot_config/nvim/lua/helpers.lua
+++ home/private_dot_config/nvim/lua/helpers.lua
@@ -1,5 +1,28 @@
 local M = {}
 
+M.execute_cmd = function(cmd)
+    local handle = io.popen(cmd)
+    local result = ''
+    if handle then
+        result = handle:read('*a')
+        handle:close()
+    end
+    result = string.gsub(result, '%s+$', '')
+    return result
+end
+
+M.flatten = function(tbl, result)
+    result = result or {}
+    for _, v in ipairs(tbl) do
+        if type(v) == "table" then
+            M.flatten(v, result)
+        else
+            table.insert(result, v)
+        end
+    end
+    return result
+end
+
 M.get_listed_buffers = function()
     local buffers = {}
     for _, buffer in ipairs(vim.api.nvim_list_bufs()) do
@@ -11,36 +34,39 @@ M.get_listed_buffers = function()
     return buffers
 end
 
-M.tbl_tostring = function(tbl, indent, is_nest)
-    local newline = '\n'
-    if vim.fn.has('unix') ~= 1 then
-        newline = '\r\n'
+M.get_mode_name = function()
+    local mode = vim.fn.mode():match('^.')
+    local mode_name = 'normal'
+    if mode == 'n' then
+        mode_name = 'normal'
+    elseif mode == 'i' then
+        mode_name = 'insert'
+    elseif mode == 'v' or mode == 'V' or mode == '\19' or mode == '\22' or mode == 's' or mode == 'S' then
+        mode_name = 'visual'
+    elseif mode == 'c' then
+        mode_name = 'command'
+    elseif mode == 't' then
+        mode_name = 'terminal'
+    elseif mode == 'r' or mode == 'R' then
+        mode_name = 'replace'
     end
-    if not indent then indent = 0 end
-    local toprint = (is_nest and '' or string.rep(' ', indent)) .. '{' .. newline
-    indent = indent + 2
-    for k, v in pairs(tbl) do
-        toprint = toprint .. string.rep(' ', indent)
-        if (type(k) == 'number') then
-            toprint = toprint .. '[' .. k .. '] = '
-        elseif (type(k) == 'string') then
-            toprint = toprint .. k .. '= '
-        end
-        if (type(v) == 'number') then
-            toprint = toprint .. v .. ',' .. newline
-        elseif (type(v) == 'string') then
-            toprint = toprint .. '\"' .. v .. '\",' .. newline
-        elseif (type(v) == 'table') then
-            toprint = toprint .. M.tbl_tostring(v, indent + 2, true) .. ',' .. newline
-        else
-            toprint = toprint .. '\"' .. tostring(v) .. '\",' .. newline
-        end
-    end
-    toprint = toprint .. string.rep(' ', indent - 2) .. '}'
-    return toprint
+    return mode_name
 end
-M.tbl_print = function(tbl)
-    print(M.tbl_tostring(tbl))
+
+M.get_shell_path = function(fallback_shell)
+    if type(fallback_shell) ~= 'string' or fallback_shell == '' then
+        fallback_shell = '/bin/bash'
+    end
+    local bin_path = os.getenv('HOME') .. '/.local/bin/get-shell-path'
+    local shell = M.execute_cmd(bin_path .. ' 2>/dev/null')
+    if shell == '' then
+        shell = fallback_shell
+    end
+    return shell
+end
+
+M.p = function(arg)
+    vim.print(vim.inspect(arg))
 end
 
 return M
