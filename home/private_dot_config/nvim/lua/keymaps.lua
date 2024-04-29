31df034c04a2bf209d9c4fd9b928e8a748776f25
yusnked <yusnked@gmail.com>
2024-04-29 20:54:39 +0900

    [nvim] :JoinSpaceLessコマンドを作成してgJに設定・keymaps.luaの更新

--- home/private_dot_config/nvim/lua/keymaps.lua
+++ home/private_dot_config/nvim/lua/keymaps.lua
@@ -1,22 +1,42 @@
 local keymap = vim.keymap.set
+local create_command = vim.api.nvim_create_user_command
 
 -- leader key
-keymap('', '<Space>', '<Nop>')
+keymap('', '<Space>', '')
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Normal mode
 keymap('n', 'j', 'gj')
 keymap('n', 'k', 'gk')
 keymap('n', 'gj', 'j')
 keymap('n', 'gk', 'k')
-keymap('n', '<Esc><Esc>', ':<C-u>nohlsearch<CR>', { silent = true, desc = 'nohlsearch' })
+keymap({ 'n', 'x' }, 'gJ', ':JoinSpaceLess<CR>', { silent = true })
 
--- Visual mode
--- 右クリックメニューでの "E335: Menu not defined for Insert mode" を防止.
+-- Prevent error "E335: Menu not defined for Insert mode".
 keymap('v', '<RightMouse>', '<C-\\><C-g>gv<cmd>:popup! PopUp<CR>')
 
--- Command line
--- 履歴の<C-n><C-p>がnvim-cmpと競合する為
-keymap('c', '<C-j>', '<Down>')
-keymap('c', '<C-k>', '<Up>')
+keymap('c', '<C-b>', '<Left>')
+keymap('c', '<C-f>', '<Right>')
+
+-- User command
+create_command('JoinSpaceLess', function(arg)
+    local line_start = arg.line1 - 1
+    local line_end = arg.line2
+    if line_end - line_start == 1 then
+        line_end = line_end + 1
+    end
+    local joined_line = ''
+    for index, line in ipairs(vim.api.nvim_buf_get_lines(0, line_start, line_end, false)) do
+        local trimmed_line = line
+        if index ~= 1 then
+            trimmed_line, _ = line:gsub('^%s*', '')
+        end
+        joined_line = joined_line .. trimmed_line
+    end
+    vim.api.nvim_buf_set_lines(0, line_start, line_end, false, { joined_line })
+end, { range = true, desc = 'Join lines without spaces' })
+
+-- Abbreviation:
+vim.cmd([[
+    cnoreabbrev <expr> s getcmdtype() .. getcmdline() ==# ':s' ? [getchar(), ''][1] .. "%s///g<Left><Left>" : 's'
+]])
