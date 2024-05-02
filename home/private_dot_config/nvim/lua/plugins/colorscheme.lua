def6b97eaff07afe6d2e3c1a0d2782ee9cd4f0c4
yusnked <yusnked@gmail.com>
2024-05-02 17:55:10 +0900

    [nvim] カラースキームcarbonfoxのgit.changedを見やすい色に調整

--- home/private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ home/private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,25 +1,21 @@
 return {
     {
         'EdenEast/nightfox.nvim',
-        version = '*',
         lazy = false,
         priority = 1000,
         cond = NOT_VSCODE,
-        opts = function()
-            return {
-                palettes = {
-                    carbonfox = {
-                        -- status line and float
-                        bg0 = '#111111',
-                        -- default bg
-                        bg1 = '#1E1E1E',
-                        -- visual selection bg
-                        sel0 = '#505050',
-                    }
-                }
-            }
+        build = function()
+            require('nightfox').compile()
         end,
-        init = function()
+        config = function()
+            require('nightfox').setup {
+                palettes = {
+                    carbonfox = { sel0 = '#404040' },
+                },
+                specs = {
+                    carbonfox = { git = { changed = '#0081c0' } },
+                },
+            }
             vim.cmd.colorscheme('carbonfox')
         end,
     },
