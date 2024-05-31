f2f01db3518852c512f7fe260b2c97b1285dfe26
yusnked <yusnked@gmail.com>
2024-06-01 06:17:59 +0900

    [nvim] nvim-treesitterを起動時にロードするように変更・他

--- home/private_dot_config/nvim/lua/plugins/treesitter.lua
+++ home/private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,134 +1,130 @@
 return {
     {
         'nvim-treesitter/nvim-treesitter',
-        version = '*',
-        event = { 'VeryLazy' },
+        lazy = false,
         build = function()
             require('nvim-treesitter.install').update { with_sync = true }
         end,
-        main = 'nvim-treesitter.configs',
-        opts = function()
+        init = function()
             vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
                 callback = function()
                     require('helpers').exec_autocmds_by_group_pattern('^NvimTreesitter', { 'FileType' })
                 end,
                 once = true,
             })
-            return {
-                ensure_installed = {
-                    'awk',
-                    'bash',
-                    'c',
-                    -- 'comment', -- SLOW
-                    'cpp',
-                    'css',
-                    'csv',
-                    'diff',
-                    'dockerfile',
-                    'embedded_template',
-                    'git_config',
-                    'git_rebase',
-                    'gitattributes',
-                    'gitcommit',
-                    'gitignore',
-                    'go',
-                    'gpg',
-                    'html',
-                    'http',
-                    'ini',
-                    'javascript',
-                    'jq',
-                    'json',
-                    'json5',
-                    'jsonc',
-                    'lua',
-                    'make',
-                    'markdown',
-                    'markdown_inline',
-                    'nix',
-                    'passwd',
-                    'python',
-                    'regex',
-                    'ruby',
-                    'rust',
-                    'scss',
-                    'sql',
-                    'styled',
-                    'terraform',
-                    'toml',
-                    'tsv',
-                    'typescript',
-                    'vim',
-                    'vimdoc',
-                    'xml',
-                    'yaml',
-                },
-                sync_install = false,
-                auto_install = false,
-
-                highlight = {
-                    enable = NOT_VSCODE,
-                    disable = function(_, buf)
-                        local max_filesize = 100 * 1024 -- 100 KB
-                        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                        if ok and stats and stats.size > max_filesize then
-                            return true
-                        end
-                    end,
-                    additional_vim_regex_highlighting = false,
-                },
-                indent = { enable = NOT_VSCODE },
-                textobjects = {
-                    select = {
-                        enable = true,
-                        lookahead = true,
-                        keymaps = {
-                            ['aa'] = '@parameter.outer',
-                            ['ia'] = '@parameter.inner',
-                            ['ac'] = '@class.outer',
-                            ['ic'] = '@class.inner',
-                            ['af'] = '@function.outer',
-                            ['if'] = '@function.inner',
-                            ['agb'] = '@block.outer',
-                            ['igb'] = '@block.inner',
-                        },
-                    },
-                    move = {
-                        enable = true,
-                        set_jumps = true,
-                        goto_next_start = {
-                            [']m'] = '@function.outer',
-                        },
-                        goto_next_end = {
-                            [']M'] = '@function.outer',
-                        },
-                        goto_previous_start = {
-                            ['[m'] = '@function.outer',
-                        },
-                        goto_previous_end = {
-                            ['[M'] = '@function.outer',
-                        },
-                    },
-                },
-                -- matchup = { enable = true },
-            }
         end,
+        main = 'nvim-treesitter.configs',
+        opts = {
+            ensure_installed = {
+                'awk',
+                'bash',
+                'c',
+                -- 'comment', -- https://github.com/neovim/neovim/issues/22426
+                'cpp',
+                'css',
+                'csv',
+                'diff',
+                'dockerfile',
+                'embedded_template',
+                'git_config',
+                'git_rebase',
+                'gitattributes',
+                'gitcommit',
+                'gitignore',
+                'go',
+                'gpg',
+                'html',
+                'http',
+                'ini',
+                'javascript',
+                'jq',
+                'json',
+                'json5',
+                'jsonc',
+                'lua',
+                'make',
+                'markdown',
+                'markdown_inline',
+                'nix',
+                'passwd',
+                'python',
+                'regex',
+                'ruby',
+                'rust',
+                'scss',
+                'sql',
+                'styled',
+                'terraform',
+                'toml',
+                'tsv',
+                'typescript',
+                'vim',
+                'vimdoc',
+                'xml',
+                'yaml',
+            },
+            sync_install = false,
+            auto_install = false,
+
+            highlight = {
+                enable = NOT_VSCODE,
+                disable = function(_, buf)
+                    local max_filesize = 100 * 1024 -- 100 KB
+                    local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
+                    if ok and stats and stats.size > max_filesize then
+                        return true
+                    end
+                end,
+                additional_vim_regex_highlighting = false,
+            },
+            indent = { enable = NOT_VSCODE },
+            textobjects = {
+                select = {
+                    enable = true,
+                    lookahead = true,
+                    keymaps = {
+                        ['aa'] = '@parameter.outer',
+                        ['ia'] = '@parameter.inner',
+                        ['ac'] = '@class.outer',
+                        ['ic'] = '@class.inner',
+                        ['af'] = '@function.outer',
+                        ['if'] = '@function.inner',
+                        ['agb'] = '@block.outer',
+                        ['igb'] = '@block.inner',
+                    },
+                },
+                move = {
+                    enable = true,
+                    set_jumps = true,
+                    goto_next_start = {
+                        [']m'] = '@function.outer',
+                    },
+                    goto_next_end = {
+                        [']M'] = '@function.outer',
+                    },
+                    goto_previous_start = {
+                        ['[m'] = '@function.outer',
+                    },
+                    goto_previous_end = {
+                        ['[M'] = '@function.outer',
+                    },
+                },
+            },
+            matchup = { enable = true },
+        },
     },
     {
         'nvim-treesitter/nvim-treesitter-textobjects',
-        dependencies = { 'nvim-treesitter/nvim-treesitter' },
         event = { 'VeryLazy' },
     },
     {
         'nvim-treesitter/nvim-treesitter-context',
-        dependencies = { 'nvim-treesitter/nvim-treesitter' },
         event = { 'VeryLazy' },
         cond = NOT_VSCODE,
-        opts = {},
+        opts = { max_lines = 2 },
     },
     {
         'JoosepAlviste/nvim-ts-context-commentstring',
-        dependencies = { 'nvim-treesitter/nvim-treesitter' },
         config = function()
             vim.g.skip_ts_context_commentstring_module = true
             require('ts_context_commentstring').setup { enable_autocmd = false }
@@ -136,12 +132,10 @@ return {
     },
     {
         'HiPhish/rainbow-delimiters.nvim',
-        dependencies = { 'nvim-treesitter/nvim-treesitter' },
         cond = NOT_VSCODE,
     },
     {
         'windwp/nvim-ts-autotag',
-        dependencies = { 'nvim-treesitter/nvim-treesitter' },
         event = { 'InsertEnter *.html,*.jsx,*.tsx,*.xml' },
         cond = NOT_VSCODE,
         config = function()
@@ -153,7 +147,6 @@ return {
     },
     {
         'Wansmer/treesj',
-        dependencies = { 'nvim-treesitter/nvim-treesitter' },
         keys = { { 'J', desc = 'TSJToggle or normal! J' } },
         opts = { use_default_keymaps = false },
         config = function()
