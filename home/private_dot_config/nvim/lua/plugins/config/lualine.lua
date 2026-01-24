9b26f60bd5c260958b3dba87d3446b21ffebf9e3
yusnked <yusnked@gmail.com>
2026-01-24 23:09:49 +0900

    feat(nvim): lualine の diff コンポーネントの設定を追加

--- home/private_dot_config/nvim/lua/plugins/config/lualine.lua
+++ home/private_dot_config/nvim/lua/plugins/config/lualine.lua
@@ -38,6 +38,22 @@ function M.setup()
     end)
 
     local winbar = {
+        lualine_b = {
+            {
+                "diff",
+                source = function()
+                    local gitsigns = vim.b.gitsigns_status_dict
+                    if gitsigns then
+                        return {
+                            added = gitsigns.added,
+                            modified = gitsigns.changed,
+                            removed = gitsigns.removed,
+                        }
+                    end
+                    return nil
+                end,
+            },
+        },
         lualine_c = {
             { "%n" },
             { "filesize", separator = "", padding = { left = 1 } },
