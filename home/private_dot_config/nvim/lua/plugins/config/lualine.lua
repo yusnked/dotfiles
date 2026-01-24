a5c19026b33032323325de1ee24a545d36e870d9
yusnked <yusnked@gmail.com>
2026-01-24 21:33:33 +0900

    chore(nvim): lualine.nvim の セパレータ文字の変更

--- home/private_dot_config/nvim/lua/plugins/config/lualine.lua
+++ home/private_dot_config/nvim/lua/plugins/config/lualine.lua
@@ -1,7 +1,17 @@
 local M = {}
 
+local tabpagenr = vim.fn.tabpagenr
+
 local function cond_encoding()
     return not (vim.bo.fileencoding == "utf-8" and not vim.bo.bomb)
+end
+
+local function separator_factory(fn)
+    return {
+        fn,
+        separator = "",
+        padding = 0,
+    }
 end
 
 function M.setup()
@@ -11,6 +21,21 @@ function M.setup()
     local lualine = require("lualine")
     local selcount_var = "lualine_selcount"
     local abbrev_cwd_var = "lualine_abbrev_cwd"
+
+    local left_hard_sep = ""
+    local right_hard_sep = ""
+    local left_soft_sep = ""
+    local right_soft_sep = ""
+
+    local leftmost_sep_component = separator_factory(function()
+        return left_hard_sep
+    end)
+    local rightmost_sep_component = separator_factory(function()
+        return right_hard_sep
+    end)
+    local tab_leftmost_sep_component = separator_factory(function()
+        return tabpagenr() == 1 and left_hard_sep or "█"
+    end)
 
     local winbar = {
         lualine_c = {
@@ -38,20 +63,25 @@ function M.setup()
                 statusline = {},
                 winbar = { "man", "qf" },
             },
+            component_separators = { left = left_soft_sep, right = right_soft_sep },
+            section_separators = { left = left_hard_sep, right = right_hard_sep },
         },
         sections = {
-            lualine_a = { "mode" },
+            lualine_a = { leftmost_sep_component, { "mode" } },
             lualine_b = { "w:" .. selcount_var, "searchcount" },
             lualine_c = {},
             lualine_x = {},
             lualine_y = { "branch" },
-            lualine_z = { "w:" .. abbrev_cwd_var },
-
+            lualine_z = {
+                { "w:" .. abbrev_cwd_var, separator = "" },
+                rightmost_sep_component,
+            },
         },
         winbar = winbar,
         inactive_winbar = winbar,
         tabline = {
             lualine_a = {
+                tab_leftmost_sep_component,
                 {
                     "tabs",
                     mode = 2,
