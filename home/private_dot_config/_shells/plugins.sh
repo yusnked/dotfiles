789b9e1b8a774b773101f7c7a860229feb8a1a74
yusnked <yusnked@gmail.com>
2026-05-15 09:38:04 +0900

    chore(zsh): sheldon 設定を削除
    zsh では antidote へ移行し, bash ではプラグインを使わない為削除.

--- home/private_dot_config/_shells/plugins.sh
+++ home/private_dot_config/_shells/plugins.sh
@@ -1,27 +1,3 @@
-if type sheldon &>/dev/null; then
-    function _sheldon-settings() {
-        if [[ -n $ZSH_VERSION ]]; then
-            local shell_type='zsh'
-        elif [[ -n $BASH_VERSION ]]; then
-            local shell_type='bash'
-        else
-            return 1
-        fi
-
-        export SHELDON_CONFIG_DIR="$DOTS_CONFIG_HOME/sheldon"
-        export SHELDON_CONFIG_FILE="$SHELDON_CONFIG_DIR/plugins.toml"
-        local cache_dir="$DOTS_CACHE_HOME/sheldon"
-        local cache_file="$cache_dir/${shell_type}-plugins.sh"
-        if [[ ! -r $cache_file || $SHELDON_CONFIG_FILE -nt $cache_file ]]; then
-            mkdir -p "$cache_dir"
-            sheldon --profile $shell_type source >"$cache_file"
-        fi
-        source "$cache_file"
-    }
-    _sheldon-settings
-    unset -f _sheldon-settings
-fi
-
 # fzf (NEED: export RUNEWIDTH_EASTASIAN=0)
 export FZF_DEFAULT_COMMAND='fd --color always --unrestricted -E .git -E .DS_Store -E ".Trash-*"'
 export FZF_DEFAULT_OPTS="--ansi --cycle --exact --multi
