58ca19dc8c582cbad748636ab8dfb0e32664eb65
yusnked <yusnked@gmail.com>
2026-05-19 08:09:36 +0900

    feat(zsh): powerlevel10k とフォールバックプロンプトの追加
    - powerlevel10k プラグインを追加.
    - プロンプト設定を rc/prompt.zsh に追加.
    - promptinit / prompt システムでプロンプトを切り替え.
    - dots_fallback プロンプトテーマを追加.
    - pipestatus を表示する prompt_pipestatus を追加.

---
+++ home/private_dot_config/zsh/rc/prompt.zsh
@@ -0,0 +1,16 @@
+autoload -Uz promptinit && promptinit
+
+if [[ "$FPATH" == *powerlevel10k* ]]; then
+    prompt powerlevel10k
+    if [[ -r "$ZDOTDIR/.p10k.zsh" ]]; then
+        source "$ZDOTDIR/.p10k.zsh"
+    else
+        p10k configure
+    fi
+
+    # git のマークを消す.
+    typeset -g POWERLEVEL9K_VCS_VISUAL_IDENTIFIER_EXPANSION=
+    typeset -g POWERLEVEL9K_VCS_LOADING_VISUAL_IDENTIFIER_EXPANSION=
+else
+    prompt dots_fallback
+fi
