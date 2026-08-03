f3a086f5f8ce35a0e92f1a1438f1868e441ffae1
yusnked <yusnked@gmail.com>
2026-08-03 09:44:22 +0900

    feat(nvim): fyler 以外のウィンドウが1つのとき quit で終了・他
    - <leader>e で開いた fyler finder 以外のウィンドウが1つのときに
      そのウィンドウで quit が実行されたときに終了するようにした.
    - finder を開いたときのディレクトリである root directory に移動する
      = mapping を追加.
    - require('fyler.finder') をプロキシ経由で呼ぶことでトップレベルに移動.

--- home/private_dot_config/nvim/lua/plugins/config/fyler.lua
+++ home/private_dot_config/nvim/lua/plugins/config/fyler.lua
@@ -2,6 +2,10 @@ local M = {}
 
 local fyler = require('fyler')
 
+-- fyler.finder は setup 後じゃないと呼べないのでプロキシ経由で呼ぶ.
+---@module 'fyler.finder'
+local fyler_finder = fyler.import('fyler.finder')
+
 local map = require('plugins.util.keydesc').set
 
 local function fyler_width()
@@ -10,7 +14,7 @@ end
 
 ---@param self fyler.Finder
 local function get_cursor_path(self)
-    local entry = require('fyler.finder').parse_cursor_line(self)
+    local entry = fyler_finder.parse_cursor_line(self)
     if not entry then return end
 
     return entry.path
@@ -32,7 +36,6 @@ function M.config()
         mappings = {
             n = {
                 ['.'] = map_disabled,
-                ['='] = map_disabled,
                 ['g.'] = map_disabled,
                 ['gi'] = map_disabled,
                 ['q'] = map_disabled,
@@ -53,6 +56,10 @@ function M.config()
                     action = 'visit',
                     args = { parent = true },
                     opts = { desc = 'Go to parent directory (fyler)' },
+                },
+                ['='] = {
+                    action = 'visit',
+                    opts = { desc = 'Go to root directory (fyler)' },
                 },
                 ['<2-LeftMouse>'] = map_open,
                 ['<CR>'] = map_open,
@@ -111,7 +118,44 @@ function M.config()
     }
     fyler.setup(opts)
 
-    map('n', '<leader>e', function() fyler.open { width = fyler_width() } end)
+    map('n', '<leader>e', function()
+        fyler.open { width = fyler_width() }
+
+        -- fyler 以外のウィンドウが1つで, そのウィンドウで quit が実行された時,
+        -- fyler ウィンドウでも quit を実行する.
+        vim.defer_fn(function()
+            -- Finderインスタンスはタブごとに最大1つ.
+            -- Finderを閉じて再度開くたびにバッファが再作成され, buf_idも変わる.
+            local tab = vim.api.nvim_get_current_tabpage()
+            local finder = fyler_finder.instance_get_or_nil(tab)
+            if finder == nil then
+                vim.notify('Failed to get the Fyler finder instance for the current tab.',
+                    vim.log.levels.WARN,
+                    { title = 'fyler (<leader>e)' })
+                return
+            end
+
+            vim.api.nvim_create_autocmd('BufEnter', {
+                group = vim.api.nvim_create_augroup('plugins_fyler_quit', {}),
+                buf = finder.buf_id,
+                callback = function()
+                    -- NOTE: EXTRACT get_normal_wins()
+                    local normal_wins = vim.iter(vim.api.nvim_tabpage_list_wins(0))
+                        :filter(function(win)
+                            return vim.api.nvim_win_get_config(win).relative == ''
+                        end)
+                        :totable()
+                    if #normal_wins ~= 1 then return end
+
+                    local prev_cmd = vim.fn.histget('cmd', -1)
+                    if vim.regex([[\v^(q%[uit]!?|wq)$]]):match_str(prev_cmd) then
+                        vim.cmd.quit()
+                    end
+                end,
+                desc = 'Quit when Fyler is the last window',
+            })
+        end, 0)
+    end)
     map('n', '<leader>E', fyler.close)
 end
 
