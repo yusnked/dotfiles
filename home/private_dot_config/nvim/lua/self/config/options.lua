29e2fa51214a100297814a37a37b11d05a49f4e1
yusnked <yusnked@gmail.com>
2026-08-23 00:29:01 +0900

    feat(nvim): Option の設定の追加・見直し
    - confirm, jumpoptions, helplang の設定を追加.
    - 基本的にデフォルトで設定されているものを削除.
    - 設定の並び順を調整.

--- home/private_dot_config/nvim/lua/self/config/options.lua
+++ home/private_dot_config/nvim/lua/self/config/options.lua
@@ -2,15 +2,13 @@
 -- =vim.api.nvim_get_option_info2('{option_name}', {})
 local opt = vim.opt
 
-opt.fileencoding = 'utf-8'
 opt.fileencodings = { 'ucs-bom', 'utf-8', 'cp932', 'default', 'latin1' }
 
+opt.confirm = true
 opt.mouse = 'a'
-opt.belloff = 'all'
 opt.shortmess:append('aI')
 
 opt.number = true
-opt.numberwidth = 4
 opt.relativenumber = true
 opt.cursorline = true
 opt.cursorlineopt = 'number'
@@ -27,29 +25,29 @@ opt.listchars = {
     'trail:-',
 }
 opt.ambiwidth = 'single'
+
 opt.scrolloff = 3
 opt.sidescrolloff = 3
+opt.wrap = false
+opt.virtualedit = 'block' -- Visual blockで仮想空白を許可.
 
 opt.expandtab = true
-opt.tabstop = 8           -- Tab文字の表示幅. 互換性の為8固定推奨(らしい)
+opt.tabstop = 8      -- Tab文字の表示幅. 互換性の為8固定推奨(らしい)
 opt.shiftwidth = 4
-opt.softtabstop = -1      -- 負の数指定でshiftwidthの値を使用.
-
-opt.virtualedit = 'block' -- Visual blockで仮想空白を許可.
-opt.wrap = false
+opt.softtabstop = -1 -- 負の数指定でshiftwidthの値を使用.
 
 opt.splitbelow = true
 opt.splitright = true
 
 opt.ignorecase = true
 opt.smartcase = true
-opt.hlsearch = true
-opt.incsearch = true
 opt.inccommand = 'split'
 if vim.fn.executable('rg') == 1 then
     opt.grepprg = "rg --vimgrep --smart-case --hidden --no-messages --glob '!.git/*'"
     opt.grepformat = '%f:%l:%c:%m'
 end
+
+opt.jumpoptions = { 'clean', 'stack', 'view' }
 
 opt.wildmode = { 'longest:full', 'full' }
 opt.wildignorecase = true
@@ -66,7 +64,6 @@ opt.wildignore:append {
 opt.wildoptions:append('fuzzy')
 
 opt.completeopt = { 'menuone', 'noselect', 'popup', 'fuzzy' }
-
 opt.pumheight = 15
 opt.pumwidth = 20
 opt.pummaxwidth = 60
@@ -86,3 +83,5 @@ opt.undofile = true  -- 永続 Undo.
 
 opt.timeoutlen = 800 -- マッピングの待ち時間.
 opt.updatetime = 1000
+
+opt.helplang = 'ja'
